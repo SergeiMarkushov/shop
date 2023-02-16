918dfb9a22c3fce1a1234ddd15011df160f6624f
SeregaMarkush <s.markushov@icloud.com>
2023-02-16 12:25:38 +0300

    added pattern Builder to some classes

--- api/src/main/java/ru/gb/shop/api/CartItemDto.java
+++ api/src/main/java/ru/gb/shop/api/CartItemDto.java
@@ -9,55 +9,92 @@ public class CartItemDto {
     private BigDecimal pricePerProduct;
     private BigDecimal price;
 
-
-    public Long getProductId() {
-        return productId;
+    private CartItemDto(Builder builder) {
+        productId = builder.productId;
+        productTitle = builder.productTitle;
+        quantity = builder.quantity;
+        pricePerProduct = builder.pricePerProduct;
+        price = builder.price;
     }
 
-    public void setProductId(Long productId) {
-        this.productId = productId;
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+    public static Builder newBuilder(CartItemDto copy) {
+        Builder builder = new Builder();
+        builder.productId = copy.getProductId();
+        builder.productTitle = copy.getProductTitle();
+        builder.quantity = copy.getQuantity();
+        builder.pricePerProduct = copy.getPricePerProduct();
+        builder.price = copy.getPrice();
+        return builder;
     }
 
-    public String getProductTitle() {
+    private String getProductTitle() {
         return productTitle;
     }
 
-    public void setProductTitle(String productTitle) {
-        this.productTitle = productTitle;
+
+    public Long getProductId() {
+        return productId;
     }
 
     public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
+
 
     public BigDecimal getPricePerProduct() {
         return pricePerProduct;
     }
 
-    public void setPricePerProduct(BigDecimal pricePerProduct) {
-        this.pricePerProduct = pricePerProduct;
-    }
+
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
 
-    public CartItemDto(Long productId, String productTitle, int quantity, BigDecimal pricePerProduct, BigDecimal price) {
-        this.productId = productId;
-        this.productTitle = productTitle;
-        this.quantity = quantity;
-        this.pricePerProduct = pricePerProduct;
-        this.price = price;
-    }
+    public static final class Builder {
+        private Long productId;
+        private String productTitle;
+        private int quantity;
+        private BigDecimal pricePerProduct;
+        private BigDecimal price;
 
-    public CartItemDto() {
+        private Builder() {
+        }
+
+
+
+        public Builder withProductId(Long val) {
+            productId = val;
+            return this;
+        }
+
+        public Builder withProductTitle(String val) {
+            productTitle = val;
+            return this;
+        }
+
+        public Builder withQuantity(int val) {
+            quantity = val;
+            return this;
+        }
+
+        public Builder withPricePerProduct(BigDecimal val) {
+            pricePerProduct = val;
+            return this;
+        }
+
+        public Builder withPrice(BigDecimal val) {
+            price = val;
+            return this;
+        }
+
+        public CartItemDto build() {
+            return new CartItemDto(this);
+        }
     }
 }
--- api/src/main/java/ru/gb/shop/api/OrderItemDto.java
+++ api/src/main/java/ru/gb/shop/api/OrderItemDto.java
@@ -12,51 +12,93 @@ public class OrderItemDto {
     private BigDecimal pricePerProduct;
     private BigDecimal price;
 
+    private OrderItemDto(Builder builder) {
+        id = builder.id;
+        productTitle = builder.productTitle;
+        orderId = builder.orderId;
+        quantity = builder.quantity;
+        pricePerProduct = builder.pricePerProduct;
+        price = builder.price;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static Builder newBuilder(OrderItemDto copy) {
+        Builder builder = new Builder();
+        builder.id = copy.getId();
+        builder.productTitle = copy.getProductTitle();
+        builder.orderId = copy.getOrderId();
+        builder.quantity = copy.getQuantity();
+        builder.pricePerProduct = copy.getPricePerProduct();
+        builder.price = copy.getPrice();
+        return builder;
+    }
+
+
     public Long getId() {
         return id;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getProductTitle() {
         return productTitle;
     }
-
-    public void setProductTitle(String productTitle) {
-        this.productTitle = productTitle;
-    }
-
     public Long getOrderId() {
         return orderId;
     }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
     public int getQuantity() {
         return quantity;
     }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
     public BigDecimal getPricePerProduct() {
         return pricePerProduct;
     }
-
-    public void setPricePerProduct(BigDecimal pricePerProduct) {
-        this.pricePerProduct = pricePerProduct;
-    }
-
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
+    public static final class Builder {
+        private Long id;
+        private String productTitle;
+        private Long orderId;
+        private int quantity;
+        private BigDecimal pricePerProduct;
+        private BigDecimal price;
+
+        private Builder() {
+        }
+
+        public Builder withId(Long val) {
+            id = val;
+            return this;
+        }
+
+        public Builder withProductTitle(String val) {
+            productTitle = val;
+            return this;
+        }
+
+        public Builder withOrderId(Long val) {
+            orderId = val;
+            return this;
+        }
+
+        public Builder withQuantity(int val) {
+            quantity = val;
+            return this;
+        }
+
+        public Builder withPricePerProduct(BigDecimal val) {
+            pricePerProduct = val;
+            return this;
+        }
+
+        public Builder withPrice(BigDecimal val) {
+            price = val;
+            return this;
+        }
+
+        public OrderItemDto build() {
+            return new OrderItemDto(this);
+        }
     }
 }
